9c429f22706788d61f94a646b8caad983c95f481
Edgar Haro <harove@gmail.com>
2023-12-24 16:48:02 -0300

    fixing admin credentials

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 export const MONGODB_CNX_STR = 'mongodb+srv://harove:harove@cluster0.56slkyh.mongodb.net/ecommerce';
 export const PORT = 8080
 export const SESSION_SECRET = 'SecretCoder'
-export const ADMIN_EMAIL = 'adminCoderno@coder.com'
+export const ADMIN_EMAIL = 'adminCoder@coder.com'
